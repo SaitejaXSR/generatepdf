7ba490de3254c13d884f9def16b57eda4a95de87
Sai teja chary <saitejachary1411@gmail.com>
2023-04-30 08:49:51 +0530

    Add lines

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ for index, row in df.iterrows():
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-    pdf.line(10, 21, 200, 21)
+    for y in range(20, 295, 5):
+        pdf.line(10, y, 200, y)
 
     # Set Footer
     pdf.ln(266)
@@ -23,7 +24,8 @@ for index, row in df.iterrows():
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
-
+        for y in range(20, 295, 5):
+            pdf.line(10, y, 200, y)
         # Set Footer
         pdf.ln(278)
         pdf.set_font(family='Times', style='I', size=8)
